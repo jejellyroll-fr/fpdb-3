f4d6e6ae9f4164278509f4d4bd39b8bfcc97f8ca
jedeopxw <jeremy.denis@weave.eu>
2022-03-02 22:54:11 +0100

    add class

---
+++ pyfpdb/OddsCalc.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+
+#use pokerprotools for odds calculation
+# g = oh5 - 5 card omaha
+# g = o85 - 5 card omaha Hi/Lo
+# g = he - holdem
+# g = oh - omaha
+# g = o8 - omaha Hi/Lo
+# g = rz - razz
+# g = st - stud
+# g = s8 - stud Hi/Lo
+
+#'s': 'generic'
+
+#'b': 'board' flop turn river
+
+#'h1' to h6 : 'hand' or range
+
+class OddsCalc:
+    def __init__(self, game, board, hero, vilain1, vilain2) -> None:
+        url = 'http://www.propokertools.com/simulations/show?'
+        params = {'g': game, 's': 'generic', 'b': board ,'h1':  hero, 'h2': vilain1}
+
+        response = requests.post(url, params=params)
+        response.status_code
+        html_doc= response.text
+
+        s = BeautifulSoup(html_doc, 'html.parser').table
+        h, [_, *d] = [i.text for i in s.tr.find_all('th')], [[i.text for i in b.find_all('td')] for b in s.find_all('tr')]
+        result = [dict(zip(h, i)) for i in d]
+        print(result)
+        
+odd = OddsCalc('he', 'As8s4d9d', 'AA', 'QQ','')
