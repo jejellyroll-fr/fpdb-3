e436c44a3db42b342dc93705fbf189458e16c006
jedeopxw <jeremy.a.a.denis@gmail.com>
2022-03-03 20:30:35 +0100

    add odds result in table
    add odds result in table

--- pyfpdb/GuiOddsCalc.py
+++ pyfpdb/GuiOddsCalc.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit,QLabel, QPushButton, QFileDialog
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit,QLabel, QPushButton, QFileDialog, QTableWidget, QTableWidgetItem
 
 import OddsCalc
 
@@ -38,9 +38,9 @@ class GuiOddsCalc(QWidget):
         self.load_button.clicked.connect(self.load_result)
         self.layout().addWidget(self.load_button)
 
-        self.QLEcalc = QLineEdit()
+        self.QTcalc = QTableWidget()
         hbox_result = QHBoxLayout()
-        hbox_result.addWidget(self.QLEcalc)
+        hbox_result.addWidget(self.QTcalc)
         self.layout().addLayout(hbox_result)
 
 
@@ -48,6 +48,21 @@ class GuiOddsCalc(QWidget):
         game = self.QLEgame.text()
         print(game)
         odd1 = OddsCalc.OddsCalc(str(game),'As8s4d9d','AK','89','') 
-        #odd1.calcBaseHoldem()
-        print(odd1.calcBaseHoldem())
-        self.QLEcalc.setText(str(odd1.calcBaseHoldem()))
+        
+        result_brut = odd1.calcBaseHoldem()
+        row_count = (len(result_brut))
+        column_count = (len(result_brut[0]))
+        self.QTcalc.setColumnCount(column_count) 
+        self.QTcalc.setRowCount(row_count)
+
+        self.QTcalc.setHorizontalHeaderLabels((list(result_brut[0].keys())))
+
+        for row in range(row_count):  # add items from array to QTableWidget
+            for column in range(column_count):
+                item = (list(result_brut[row].values())[column])
+                print (type(row))
+                print (type(column))
+                print (type(item))
+                self.QTcalc.setItem(row, column, QTableWidgetItem(item))
+    
+        
