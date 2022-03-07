6e31dcead83fc80bbf74a066ed0342690a07b70b
jedeopxw <jeremy.denis@weave.eu>
2022-03-07 22:56:43 +0100

    update add gofast in guiringplayerstat

--- pyfpdb/GuiRingPlayerStats.py
+++ pyfpdb/GuiRingPlayerStats.py
@@ -39,7 +39,7 @@ import Charset
 
 colalias,colheading,colshowsumm,colshowposn,colformat,coltype,colxalign = 0,1,2,3,4,5,6
 ranks = {'x':0, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
-fast_names = {'OnGame':'Strobe', 'PokerStars':'Zoom', 'Full Tilt Poker':'Rush', 'Bovada':'Zone', 'PacificPoker': 'Snap'}
+fast_names = {'OnGame':'Strobe', 'PokerStars':'Zoom', 'Full Tilt Poker':'Rush', 'Bovada':'Zone', 'PacificPoker': 'Snap', 'Winamax': 'Go Fast'}
 onlinehelp = {'Game':('Type of Game'),
               'Hand':('Hole Cards'),
               'Posn':('Position'),
