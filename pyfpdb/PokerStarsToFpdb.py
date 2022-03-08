eca5a98bd59bbf4a8f1fa9be97055d0914c6d554
jedeopxw <jeremy.a.a.denis@gmail.com>
2022-03-08 19:04:17 +0100

    update ps

--- pyfpdb/PokerStarsToFpdb.py
+++ pyfpdb/PokerStarsToFpdb.py
@@ -352,14 +352,14 @@ class PokerStars(HandHistoryConverter):
     def readHandInfo(self, hand):
         #First check if partial
         if hand.handText.count('*** SUMMARY ***')!=1:
-            raise FpdbHandPartial(_("Hand is not cleanly split into pre and post Summary"))
+            raise FpdbHandPartial(("Hand is not cleanly split into pre and post Summary"))
         
         info = {}
         m  = self.re_HandInfo.search(hand.handText,re.DOTALL)
         m2 = self.re_GameInfo.search(hand.handText)
         if m is None or m2 is None:
             tmp = hand.handText[0:200]
-            log.error(_("PokerStarsToFpdb.readHandInfo: '%s'") % tmp)
+            log.error(("PokerStarsToFpdb.readHandInfo: '%s'") % tmp)
             raise FpdbParseError
 
         info.update(m.groupdict())
@@ -685,7 +685,7 @@ class PokerStars(HandHistoryConverter):
             elif action.group('ATYPE') == ' stands pat':
                 hand.addStandsPat( street, action.group('PNAME'), action.group('CARDS'))
             else:
-                log.debug(_("DEBUG:") + " " + _("Unimplemented %s: '%s' '%s'") % ("readAction", action.group('PNAME'), action.group('ATYPE')))
+                log.debug(("DEBUG:") + " " + ("Unimplemented %s: '%s' '%s'") % ("readAction", action.group('PNAME'), action.group('ATYPE')))
 
 
     def readShowdownActions(self, hand):
