108aae8240318a3ce6671fdc46f524ad32da61e6
jedeopxw <jeremy.a.a.denis@gmail.com>
2022-08-31 17:52:59 +0200

    working test with ploev lib with pql

--- pyfpdb/OddsCalcnew.py
+++ pyfpdb/OddsCalcnew.py
@@ -1,4 +1,6 @@
+from unittest import result
 from ploev.calc import *
+from ploev.ppt import *
 
 # use https://github.com/vyvojer/ploev python lib for Pokerprotools odds oracle
 # until the end of 2022 the pokerprotools servers will be down, a public key is provided by the author Dan (aka nerdytenor/bachfan) to use the desktop version
@@ -20,10 +22,22 @@ from ploev.calc import *
 
 #'h1' to h6 : 'hand' or range
 
+# use pql
+
 
 odds_oracle = OddsOracle()
-calc = Calc(odds_oracle)
-test = calc.equity(players=['AcAd7h2c', 'JdTs9s8d'])
-print(test)
+pql = """
+            select count(inRange(PLAYER_1,'AA')),
+              count(inRange(PLAYER_1,'AK,KK'))
+            from game='omahahi', syntax='Generic',
+                 board='Ad Ks 3s',
+                 PLAYER_1='10%',
+                 PLAYER_2='Qs Ts Jd 2d'
+"""
+test = odds_oracle.pql(pql)
+
+print(test.results_list)
+print(type(test))
+
 #odd1 = OddsCalc('oh','As8s4d9d','AK','89','','','','','')        
 #odd1.calcBaseHoldem()
