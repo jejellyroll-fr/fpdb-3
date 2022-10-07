f1770e07a2f2dd3903eec7caa1a8ca62c6359408
jedeopxw <jeremy.a.a.denis@gmail.com>
2022-10-07 22:10:43 +0200

    plantuml and deleted c pokereval

--- pyfpdb/Mucked.py
+++ pyfpdb/Mucked.py
@@ -29,7 +29,7 @@ from __future__ import division
 
 from past.utils import old_div
 #import L10n
-_ = L10n.init_translation()
+#_ = L10n.init_translation()
 
 #    Standard Library modules
 import logging
