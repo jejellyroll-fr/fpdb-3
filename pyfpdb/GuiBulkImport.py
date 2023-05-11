e0aeb9663839c8ae6ba595fb459139d8b277c5a2
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2023-05-11 19:04:04 +0200

    refactor code
    refactor code and add allowThreads option in UI

--- pyfpdb/GuiBulkImport.py
+++ pyfpdb/GuiBulkImport.py
@@ -27,8 +27,10 @@ import sys
 from time import time
 from optparse import OptionParser
 import traceback
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QFileDialog
+
 
 #    fpdb/FreePokerTools modules
 
@@ -47,88 +49,132 @@ if __name__ == "__main__":
 log = logging.getLogger("importer")
 
 class GuiBulkImport(QWidget):
-    # CONFIGURATION  -  update these as preferred:
-    allowThreads = False  # set to True to try out the threads field
+    """
+    A widget for importing hands into the database.
+    """
+    def __init__(self, settings, config, sql=None, parent=None):
+        """
+        Initializes the ImportWidget.
 
-    def load_clicked(self):
-        stored = None
-        dups = None
-        partial = None
-        skipped = None
-        errs = None
-        ttime = None
-        # Does the lock acquisition need to be more sophisticated for multiple dirs?
-        # (see comment above about what to do if pipe already open)
-        if self.settings['global_lock'].acquire(wait=False, source="GuiBulkImport"):   # returns false immediately if lock not acquired
-            #try:
-                #    get the dir to import from the chooser
-                selected = self.importDir.text()
-
-                #    get the import settings from the gui and save in the importer
-                
-                self.importer.setHandsInDB(self.n_hands_in_db)
-                self.importer.setMode('bulk')
-
-                self.importer.addBulkImportImportFileOrDir(selected, site = 'auto')
-                self.importer.setCallHud(False)
-                
-                starttime = time()
-
-                (stored, dups, partial, skipped, errs, ttime) = self.importer.runImport()
-
-                ttime = time() - starttime
-                if ttime == 0:
-                    ttime = 1
-                    
-                completionMessage = ('Bulk import done: Stored: %d, Duplicates: %d, Partial: %d, Skipped: %d, Errors: %d, Time: %s seconds, Stored/second: %.0f')\
-                    % (stored, dups, partial, skipped, errs, ttime, old_div((stored+0.0), ttime))
-                print(completionMessage)
-                log.info(completionMessage)
-
-                self.importer.clearFileList()
-                
-                self.settings['global_lock'].release()
-        else:
-            print(("bulk import aborted - global lock not available"))
-
-    def get_vbox(self):
-        """returns the vbox of this thread"""
-        return self.layout()
-
-    def __init__(self, settings, config, sql = None, parent = None):
+        :param settings: A dictionary of settings.
+        :param config: A dictionary of configuration options.
+        :param sql: The SQL database to use.
+        :param parent: The parent widget.
+        """
+        # Call the constructor of the parent class.
         QWidget.__init__(self, parent)
+
+        # Save the settings and config as class variables.
         self.settings = settings
         self.config = config
 
+        # Set the default value for the "allowThreads" checkbox.
+        self.allowThreads = False
+
+        # Create the Importer object.
         self.importer = Importer.Importer(self, self.settings, config, sql, self)
 
+        # Set the layout of the widget to a QVBoxLayout.
         self.setLayout(QVBoxLayout())
 
+        # Add a QLineEdit for the import directory.
         self.importDir = QLineEdit(self.settings['bulkImport-defaultPath'])
         hbox = QHBoxLayout()
         hbox.addWidget(self.importDir)
+
+        # Add a QPushButton to browse for the import directory.
         self.chooseButton = QPushButton('Browse...')
         self.chooseButton.clicked.connect(self.browseClicked)
         hbox.addWidget(self.chooseButton)
         self.layout().addLayout(hbox)
 
+        # Add a QPushButton to start the import process.
         self.load_button = QPushButton(('Bulk Import'))
         self.load_button.clicked.connect(self.load_clicked)
         self.layout().addWidget(self.load_button)
 
-#    see how many hands are in the db and adjust accordingly
+        # Add a QCheckBox to allow thread usage during the import process.
+        self.threads_checkbox = QCheckBox('Allow Threads')
+        self.threads_checkbox.stateChanged.connect(self.threads_checkbox_changed)
+        self.layout().addWidget(self.threads_checkbox)
+
+        # Get the number of hands already in the database.
         tcursor = self.importer.database.cursor
         tcursor.execute("Select count(1) from Hands")
         row = tcursor.fetchone()
         tcursor.close()
+
+        # Rollback the transaction and save the number of hands in the database.
         self.importer.database.rollback()
         self.n_hands_in_db = row[0]
 
+    def threads_checkbox_changed(self, state):
+        """
+        Function to handle the state change of the threads checkbox.
+
+        Args:
+            state (Qt.CheckState): The new state of the checkbox.
+
+        Returns:
+            None
+        """
+        # Set the allowThreads attribute to True if the state is checked, False otherwise
+        self.allowThreads = state == Qt.Checked
+
+
+    def load_clicked(self):
+        """
+        Handler function for the load button click event. Loads a directory of hands into the importer.
+
+        Returns:
+            None
+        """
+        if not self.settings['global_lock'].tryLock():
+            # If global lock is not available, print error message and return
+            print("bulk import aborted - global lock not available")
+            return
+
+        selected = self.importDir.text()
+        self.importer.setHandsInDB(self.n_hands_in_db)
+        self.importer.setMode('bulk')
+        self.importer.addBulkImportImportFileOrDir(selected, site='auto')
+        self.importer.setCallHud(False)
+        self.importer.allowThreads = self.allowThreads
+
+        starttime = time()
+        stored, dups, partial, skipped, errs, ttime = self.importer.runImport()
+        ttime = time() - starttime
+
+        if ttime == 0:
+            ttime = 1
+
+        # Build completion message with import statistics
+        completionMessage = ('Bulk import done: Stored: %d, Duplicates: %d, Partial: %d, Skipped: %d, Errors: %d, Time: %s seconds, Stored/second: %.0f') \
+            % (stored, dups, partial, skipped, errs, ttime, old_div((stored + 0.0), ttime))
+
+        # Print completion message and log it
+        print(completionMessage)
+        log.info(completionMessage)
+
+        self.importer.clearFileList()
+        self.settings['global_lock'].unlock()
+
+
+
+
     def browseClicked(self):
+        """
+        Opens a QFileDialog to allow the user to select a directory to import files from.
+        Sets the import directory to the selected directory, if one is chosen.
+        """
+        # Open a QFileDialog to get the path of the directory to import from
         newdir = QFileDialog.getExistingDirectory(self, caption=("Please choose the path that you want to Auto Import"),
                                             directory=self.importDir.text())
+
+        # If a directory was selected, update the import directory text field
         if newdir:
             self.importDir.setText(newdir)
+
 
 if __name__ == '__main__':
     config = Configuration.Config()
@@ -143,10 +189,3 @@ if __name__ == '__main__':
     settings['global_lock'] = interlocks.InterProcessLock(name="fpdb_global_lock")
     settings['cl_options'] = '.'.join(sys.argv[1:])
 
-    # from PyQt5.QtWidgets import QApplication, QMainWindow
-    # app = QApplication(sys.argv)
-    # main_window = QMainWindow()
-    # main_window.setCentralWidget(GuiBulkImport(settings, config))
-    # main_window.show()
-    # main_window.resize(600, 100)
-    # app.exec_()
