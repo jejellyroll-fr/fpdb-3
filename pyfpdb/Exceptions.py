8870623bba0976197b1aa84f98441ade72f04c52
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2023-05-22 17:37:24 +0200

    refactor code
    refactor code

--- pyfpdb/Exceptions.py
+++ pyfpdb/Exceptions.py
@@ -16,66 +16,94 @@
 #In the "official" distribution you can find the license in agpl-3.0.txt.
 
 class FpdbError(Exception):
+    """Base exception class for Fpdb-related errors."""
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return repr(self.value)
 
+
 class FpdbParseError(FpdbError):
-    def __init__(self,value='',hid=''):
+    """Exception class for parsing errors in Fpdb."""
+    def __init__(self, value='', hid=''):
         self.value = value
         self.hid = hid
+
     def __str__(self):
-        if self.hid:
-            return repr("HID:"+self.hid+", "+self.value)
-        else:
-            return repr(self.value)
+        return repr(f"HID:{self.hid}, {self.value}") if self.hid else repr(self.value)
+
 
 class FpdbDatabaseError(FpdbError):
+    """Exception class for Fpdb database errors."""
     pass
+
 
 class FpdbMySQLError(FpdbDatabaseError):
+    """Exception class for MySQL database errors in Fpdb."""
     pass
+
 
 class FpdbMySQLAccessDenied(FpdbDatabaseError):
+    """Exception class for MySQL access denied errors in Fpdb."""
     def __init__(self, value='', errmsg=''):
         self.value = value
         self.errmsg = errmsg
+
     def __str__(self):
-        return repr(self.value +" " + self.errmsg)
+        return repr(f"{self.value} {self.errmsg}")
+
 
 class FpdbMySQLNoDatabase(FpdbDatabaseError):
+    """Exception class for MySQL database not found errors in Fpdb."""
     def __init__(self, value='', errmsg=''):
         self.value = value
         self.errmsg = errmsg
+
     def __str__(self):
-        return repr(self.value +" " + self.errmsg)
+        return repr(f"{self.value} {self.errmsg}")
+
 
 class FpdbPostgresqlAccessDenied(FpdbDatabaseError):
+    """Exception class for PostgreSQL access denied errors in Fpdb."""
     def __init__(self, value='', errmsg=''):
         self.value = value
         self.errmsg = errmsg
+
     def __str__(self):
-        return repr(self.value +" " + self.errmsg)
+        return repr(f"{self.value} {self.errmsg}")
+
 
 class FpdbPostgresqlNoDatabase(FpdbDatabaseError):
+    """Exception class for PostgreSQL database not found errors in Fpdb."""
     def __init__(self, value='', errmsg=''):
         self.value = value
         self.errmsg = errmsg
+
     def __str__(self):
-        return repr(self.value +" " + self.errmsg)
+        return repr(f"{self.value} {self.errmsg}")
+
 
 class FpdbHandError(FpdbError):
+    """Exception class for Fpdb poker hand errors."""
     pass
+
 
 class FpdbHandDuplicate(FpdbHandError):
+    """Exception class for duplicate poker hands in Fpdb."""
     pass
+
 
 class FpdbHandPartial(FpdbParseError):
+    """Exception class for partially parsed poker hands in Fpdb."""
     pass
+
 
 class FpdbHandSkipped(FpdbParseError):
+    """Exception class for skipped poker hands in Fpdb."""
     pass
 
+
 class FpdbEndOfFile(FpdbHandError):
+    """Exception class for end-of-file errors in Fpdb."""
     pass
