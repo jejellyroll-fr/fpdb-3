c5a219b6eb1051af1f4f5c72c3b0bbc73877b181
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2023-05-22 17:05:22 +0200

    refactor code
    refactor code

--- pyfpdb/Card.py
+++ pyfpdb/Card.py
@@ -45,7 +45,7 @@ card_map_abbr = [
                  ["A2o","K2o","Q2o","J2o","T2o","92o","82o","72o","62o","52o","42o","32o","22" ],
                 ]
 
-games = {              # base, category
+games = {              # base, category, Format, Rounds
              'holdem' : ('hold','holdem', 'h', {'PREFLOP':0,'FLOP':1,'TURN':2,'RIVER':3}, 'RIVER', [(0,2)]), 
            '2_holdem' : ('hold', None, 'h', {'PREFLOP':0,'FLOP':1,'TURN':2,'RIVER':3}, 'RIVER', [(0,3)]),
            '6_holdem' : ('hold', None, 'h', {'PREFLOP':0,'FLOP':1,'TURN':2,'RIVER':3}, 'RIVER', [(0,2)]), 
@@ -109,6 +109,16 @@ iter = {0: 50000,
         }
 
 def decodeStartHandValue(game, value):
+    """
+    Given a game and start hand value, decode the value into a human-readable string representation.
+
+    Args:
+        game (str): The name of the game being played.
+        value (int): The encoded start hand value.
+
+    Returns:
+        str: The human-readable string representation of the start hand.
+    """
     if game in ('holdem', '6_holdem'):
         return twoStartCardString(value)
     elif game in ("razz", "27_razz"):
@@ -117,12 +127,27 @@ def decodeStartHandValue(game, value):
         return "xx"
 
 def calcStartCards(hand, player):
+    """
+    Calculates the start cards value for a given hand and player.
+
+    Args:
+        hand (Hand): The hand being evaluated.
+        player (int): The player number (0 or 1).
+
+    Returns:
+        int: The start cards value.
+    """
+    # Get the list of hole cards for the specified player
     hcs = hand.join_holecards(player, asList=True)
+
+    # Calculate the start cards value based on the game type
     if hand.gametype['category'] in ('holdem', '6_holdem'):
+        # For holdem and 6_holdem, use the twoStartCards function
         value1 = card_map.get(hcs[0][0])
         value2 = card_map.get(hcs[1][0])
         return twoStartCards(value1, hcs[0][1], value2, hcs[1][1])
     elif hand.gametype['category'] in ('razz', '27_razz'):
+        # For razz and 27_razz, use the encodeRazzStartHand function
         idx = encodeRazzStartHand(hcs)
         return idx + 184
     else:
@@ -132,7 +157,7 @@ def calcStartCards(hand, player):
 
 # The following depends on the exact implementation of twoStartCards.
 _firstcard = '((hp.startcards - 1) /  13)'
-_secondcard = '((hp.startcards - 1) - 13 * %s)' % _firstcard
+_secondcard = f'((hp.startcards - 1) - 13 * {_firstcard})'
 _gap = '(%s - %s = %d)'
 
 DATABASE_FILTERS = {
@@ -144,6 +169,15 @@ DATABASE_FILTERS = {
 }
 
 def StartCardRank(idx):
+    """
+    Returns a tuple containing the rank, high card value, and kicker value for a given index.
+
+    Args:
+        idx (int): Index of the desired rank in the rankList.
+
+    Returns:
+        Tuple[str, int, int]: A tuple containing the rank, high card value, and kicker value.
+    """
     rankList =  [
         ('22',54,12),('32o',160,24),('42o',157,24),('52o',154,24),('62o',164,24),('72o',169,24),('82o',168,24),('92o',166,24),('T2o',161,24),('J2o',153,24),('Q2o',145,24),('K2o',134,24),('A2o',113,24),
         ('32s',111,8),('33',53,12),('43o',146,24),('53o',140,24),('63o',151,24),('73o',162,24),('83o',167,24),('93o',165,24),('T3o',159,24),('J3o',152,24),('Q3o',144,24),('K3o',132,24),('A3o',105,24),
@@ -162,12 +196,13 @@ def StartCardRank(idx):
     return rankList[idx]
 
 def twoStartCards(value1, suit1, value2, suit2):
-    """ Function to convert 2 value,suit pairs into a Holdem style starting hand e.g. AQo
-        Incoming values should be ints 2-14 (2,3,...K,A), suits are 'd'/'h'/'c'/'s'
-        Hand is stored as an int 13 * x + y + 1 where (x+2) represents rank of 1st card and
-        (y+2) represents rank of second card (2=2 .. 14=Ace)
-        If x > y then pair is suited, if x < y then unsuited
-        Examples:
+    """ 
+    Function to convert 2 value,suit pairs into a Holdem style starting hand e.g. AQo
+    Incoming values should be ints 2-14 (2,3,...K,A), suits are 'd'/'h'/'c'/'s'
+    Hand is stored as an int 13 * x + y + 1 where (x+2) represents rank of 1st card and
+    (y+2) represents rank of second card (2=2 .. 14=Ace)
+    If x > y then pair is suited, if x < y then unsuited
+    Examples:
            1  22
            2  32o
            3  42o
@@ -180,15 +215,19 @@ def twoStartCards(value1, suit1, value2, suit2):
          170  Unknown / Illegal cards
     """
     if value1 is None or value1 < 2 or value1 > 14 or value2 is None or value2 < 2 or value2 > 14:
+        # Unknown / Illegal cards
         ret = 170
     elif value1 == value2: # pairs
+        # Pairs
         ret = (13 * (value2-2) + (value2-2) ) + 1
     elif suit1 == suit2:
+        # Suited
         if value1 > value2:
             ret = 13 * (value1-2) + (value2-2) + 1
         else:
             ret = 13 * (value2-2) + (value1-2) + 1
     else:
+        # Unsuited
         if value1 > value2:
             ret = 13 * (value2-2) + (value1-2) + 1
         else:
@@ -198,40 +237,84 @@ def twoStartCards(value1, suit1, value2, suit2):
     return ret
 
 def twoStartCardString(card):
-    """ Function to convert an int representing 2 holdem hole cards (as created by twoStartCards)
-        into a string like AQo """
+    """
+    Convert an int representing 2 holdem hole cards (as created by twoStartCards) into a string like AQo.
+
+    Args:
+        card (int): an integer representing 2 holdem hole cards.
+
+    Returns:
+        str: a string representing the holdem hole cards.
+    """
+    # Initialize the return string with 'xx'
     ret = 'xx'
+
+    # Check if the card is a valid holdem hole card
     if card > 0 and card < 170:
+        # Create a list of card ranks
         s = ('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
-        x = old_div((card-1), 13)
-        y = (card-1) - 13 * x
-        if x == y:  ret = s[x] + s[y]
-        elif x > y: ret = s[x] + s[y] + 's'
-        else:       ret = s[y] + s[x] + 'o'
-    print ("twoStartCardString(", card ,") = " + ret)
+
+        # Calculate the rank of each card based on the card value
+        x = (card - 1) // 13
+        y = (card - 1) % 13
+
+        # Determine the type of holdem hole card and update the return string
+        if x == y:
+            ret = s[x] + s[y]
+        elif x > y:
+            ret = s[x] + s[y] + 's'
+        else:
+            ret = s[y] + s[x] + 'o'
+
+    # Print the result for debugging purposes
+    print("twoStartCardString(", card, f") = {ret}")
+
     return ret
 
+
 def cardFromValueSuit(value, suit):
-    """ 0=none, 1-13=2-Ah 14-26=2-Ad 27-39=2-Ac 40-52=2-As """
-    if suit == 'h':  return(value-1)
-    elif suit == 'd':  return(value+12)
-    elif suit == 'c':  return(value+25)
-    elif suit == 's':  return(value+38)
-    else: return(0)
+    """
+    Given a card value and suit, returns the corresponding card ID.
+
+    Args:
+        value (int): The card value. 1-13 represents 2-Ace.
+        suit (str): The card suit. 'h' represents hearts, 'd' represents diamonds,
+                    'c' represents clubs, and 's' represents spades.
+
+    Returns:
+        int: The ID of the corresponding card. 0 represents no card, and IDs 
+             1-52 represent cards from 2-Ace of hearts, diamonds, clubs, and spades.
+    """
+    if suit == 'h':
+        return value - 1
+    elif suit == 'd':
+        return value + 12
+    elif suit == 'c':
+        return value + 25
+    elif suit == 's':
+        return value + 38
+    else:
+        return 0
+
 
 suitFromCardList = ['', '2h', '3h', '4h', '5h', '6h', '7h', '8h', '9h', 'Th', 'Jh', 'Qh', 'Kh', 'Ah'
                      , '2d', '3d', '4d', '5d', '6d', '7d', '8d', '9d', 'Td', 'Jd', 'Qd', 'Kd', 'Ad'
                      , '2c', '3c', '4c', '5c', '6c', '7c', '8c', '9c', 'Tc', 'Jc', 'Qc', 'Kc', 'Ac'
                      , '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', 'Ts', 'Js', 'Qs', 'Ks', 'As'
                 ]
+
 def valueSuitFromCard(card):
-    """ Function to convert a card stored in the database (int 0-52) into value
-        and suit like 9s, 4c etc """
-    global suitFromCardList
-    if card < 0 or card > 52 or not card:
-        return('')
-    else:
-        return suitFromCardList[card]
+    """Converts a card stored in the database (int 0-52) into value and suit like 9s, 4c etc.
+
+    Args:
+        card (int): An integer representing a card in the range [0, 52].
+
+    Returns:
+        str: A string representing the card's value and suit, or an empty string if the input is invalid.
+    """
+    global suitFromCardList  # Global variable containing a mapping of card integers to suit strings.
+    return '' if card < 0 or card > 52 or not card else suitFromCardList[card]  # Return the suit string corresponding to the input card integer, or an empty string if the input is invalid.
+
 
 encodeCardList = {'2h':  1, '3h':  2, '4h':  3, '5h':  4, '6h':  5, '7h':  6, '8h':  7, '9h':  8, 'Th':  9, 'Jh': 10, 'Qh': 11, 'Kh': 12, 'Ah': 13,
                   '2d': 14, '3d': 15, '4d': 16, '5d': 17, '6d': 18, '7d': 19, '8d': 20, '9d': 21, 'Td': 22, 'Jd': 23, 'Qd': 24, 'Kd': 25, 'Ad': 26,
@@ -241,11 +324,31 @@ encodeCardList = {'2h':  1, '3h':  2, '4h':  3, '5h':  4, '6h':  5, '7h':  6, '8
                 }
 
 def encodeCard(cardString):
-    """Take a card string (Ah) and convert it to the db card code (1)."""
+    """
+    Take a card string (e.g. Ah) and convert it to the db card code (e.g. 1).
+
+    Args:
+    - cardString (str): A string representing a card in the format 'XY', where X is the rank and Y is the suit.
+
+    Returns:
+    - int: The integer code that corresponds to the input cardString, or 0 if the input is not a valid card.
+    """
     global encodeCardList
+    # Return the integer code that corresponds to the input cardString, or 0 if the input is not a valid card.
     return encodeCardList.get(cardString, 0)
 
+
 def decodeRazzStartHand(idx):
+    """
+    Decode the Razz starting hand index and return the corresponding hand representation.
+
+    Args:
+        idx (int): The index of the Razz starting hand.
+
+    Returns:
+        str: The decoded representation of the Razz starting hand.
+
+    """
     decodeRazzList = {
     -13:'(00)A',-12:'(00)2',-11:'(00)3',-10:'(00)4',-9:'(00)5',-8:'(00)6',-7:'(00)7',-6:'(00)8',-5:'(00)9',-4:'(00)T',
     -3:'(00)J',-2:'(00)Q',-1:'(00)K',0:'xxx',
@@ -372,7 +475,15 @@ def decodeRazzStartHand(idx):
     return decodeRazzList[idx]
 
 def encodeRazzStartHand(cards):
-    """Take Razz starting hand and return an integer index for storing in db"""
+    """
+    Take Razz starting hand and return an integer index for storing in db
+
+    Parameters:
+    cards (list): A list of three tuples representing the cards in the starting hand.
+
+    Returns:
+    int: The integer index representing the starting hand for storing in the database.
+    """
     startHand = ""
     if card_map_low[cards[0][0]] > card_map_low[cards[1][0]]:
         startHand = "(%s%s)%s" %(cards[0][0], cards[1][0], cards[2][0])
@@ -516,11 +627,11 @@ if __name__ == '__main__':
     if s == '1':
         while True:
             cardid = eval(input(('Enter list ID:')+" "))
-            print(("Card:")+" '%s'" % suitFromCardList[int(cardid)])
+            print(f"Card: '{suitFromCardList[int(cardid)]}'")
     elif s == '2':
         while True:
             cardid = eval(input(('Enter card:')+" "))
-            print(("Encoded card:")+" '%s'" % encodeCard(cardid))
+            print(f"Encoded card: '{encodeCard(cardid)}'")
     else:
         print(("Please enter 1 or 2"))
 
