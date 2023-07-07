33114568f2d83ea6de8707873d7214920ee629c8
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2023-07-07 21:52:00 +0200

    update fpdb web api

--- app.py
+++ app.py
@@ -11,68 +11,29 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    context = {"request": request, "name": "World"}
-    return templates.TemplateResponse("index.html", context)
+    handscount = get_handscount()
+    handscount_cg = get_handscount_cg()
+    handscount_tour = get_handscount_tour()
+    playerscount = get_playerscount()
+    playerscount_cg = get_playerscount_cg()
+    playerscount_tour = get_playerscount_tour()
+    return templates.TemplateResponse("index.html", {"request": request, "handscount": handscount, "handscount_cg": handscount_cg, "handscount_tour": handscount_tour, "playerscount": playerscount, "playerscount_cg": playerscount_cg, "playerscount_tour": playerscount_tour})
 
-@app.get("/backings", response_class=HTMLResponse)
-async def get_backings_api(request: Request):
-    backings = get_backings()
-    return templates.TemplateResponse("backings.html", {"request": request, "backings": backings})
 
-@app.get("/actions", response_class=HTMLResponse)
-async def get_actions_api(request: Request):
-    actions = get_actions()
-    return templates.TemplateResponse("actions.html", {"request": request, "actions": actions})
-
-@app.get("/autorates", response_class=HTMLResponse)
-async def get_autorates_api(request: Request):
-    autorates = get_autorates()
-    return templates.TemplateResponse("autorates.html", {"request": request, "autorates": autorates})
-
-@app.get("/boards", response_class=HTMLResponse)
-async def get_boards_api(request: Request):
-    boards = get_boards()
-    return templates.TemplateResponse("boards.html", {"request": request, "boards": boards})
-
-@app.get("/cardsCaches", response_class=HTMLResponse)
-async def get_cardsCaches_api(request: Request):
-    cardsCaches = get_cardsCaches()
-    return templates.TemplateResponse("cardsCaches.html", {"request": request, "cardsCaches": cardsCaches})
-
-@app.get("/files", response_class=HTMLResponse)
-async def get_files_api(request: Request):
-    files = get_files()
-    return templates.TemplateResponse("files.html", {"request": request, "files": files})
-
-@app.get("/gametypes", response_class=HTMLResponse)
-async def get_gametypes_api(request: Request):
-    gametypes = get_gametypes()
-    return templates.TemplateResponse("gametypes.html", {"request": request, "gametypes": gametypes})
 
 @app.get("/hands", response_class=HTMLResponse)
 async def get_hands_api(request: Request):
     hands = get_hands()
     return templates.TemplateResponse("hands.html", {"request": request, "hands": hands})
     
-@app.get("/handsActions", response_class=HTMLResponse)
-async def get_handsActions_api(request: Request):
-    handsActions = get_handsActions()
-    return templates.TemplateResponse("handsActions.html", {"request": request, "handsActions": handsActions})
+
 
 @app.get("/handsPlayers", response_class=HTMLResponse)
 async def get_handsPlayers_api(request: Request):
     handsPlayers = get_handsPlayers()
     return templates.TemplateResponse("handsPlayers.html", {"request": request, "handsPlayers": handsPlayers})
 
-@app.get("/handsPots", response_class=HTMLResponse)
-async def get_handsPots_api(request: Request):
-    handsPots = get_handsPots()
-    return templates.TemplateResponse("handsPots.html", {"request": request, "handsPots": handsPots})
 
-@app.get("/handsStoves", response_class=HTMLResponse)
-async def get_handsStoves_api(request: Request):
-    handsStoves = get_handsStove()
-    return templates.TemplateResponse("handsStoves.html", {"request": request, "handsStoves": handsStoves})
 
 @app.get("/players", response_class=HTMLResponse)
 async def get_players_api(request: Request):
