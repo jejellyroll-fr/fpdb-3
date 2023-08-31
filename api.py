ce8e4ebb22a817f1b47f79f4e71c884acf6cf15d
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2023-08-31 18:22:24 +0200

    add dataviz to old ui
    add dataviz to old ui

--- api.py
+++ api.py
@@ -190,6 +190,6 @@ async def get_statsplayers_api(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
 
 
