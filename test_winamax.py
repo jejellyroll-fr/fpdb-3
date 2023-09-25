9d0d8b82847cb0ce70aeded09f7949843127540a
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2023-09-25 10:40:17 +0200

    fix player regex on winamax
    fix player regex on winamax

--- test_winamax.py
+++ test_winamax.py
@@ -25,6 +25,24 @@ def test_re_HandInfo():
     match = re_HandInfo.search(text)
     assert match is not None
 
+def test_re_HandInfoexp():
+    text = 'Winamax Poker - Tournament "Expresso Nitro" buyIn: 0.23€ + 0.02€ level: 1 - HandId: #3011596205705658369-1-1695541274 - Holdem no limit (10/20) - 2023/09/24 07:41:14 UTC'
+    match = re_HandInfo.search(text)
+    assert match is not None
+    assert match.group('TOURNAME') == '"Expresso Nitro"'
+    assert match.group('BUYIN') == '0.23€ + 0.02€'
+    assert match.group('BIAMT') == '0.23€'
+    assert match.group('BIRAKE') == '0.02€'
+    assert match.group('LEVEL') == '1'
+    assert match.group('HID1') == '3011596205705658369'
+    assert match.group('HID2') == '1'
+    assert match.group('HID3') == '1695541274'
+    assert match.group('GAME') == 'Holdem'
+    assert match.group('LIMIT') == 'no limit'
+    assert match.group('SB') == '10'
+    assert match.group('BB') == '20'
+    assert match.group('DATETIME') == '2023/09/24 07:41:14 UTC'
+
 
 re_HUTP = re.compile(r'Hold\-up\sto\sPot:\stotal\s((%(LS)s)?(?P<AMOUNT>[.0-9]+)(%(LS)s)?)' % substitutions, re.MULTILINE|re.VERBOSE)
 
@@ -35,4 +53,31 @@ def test_re_HUTP():
     assert match.group('AMOUNT') == '0.20'
 
 
+re_PostSB    = re.compile(r"(?P<PNAME>.*?)\sposts\ssmall\sblind\s(%(LS)s)?(?P<SB>[\.0-9]+)(%(LS)s)?(?!\sout\sof\sposition)" % substitutions, re.MULTILINE)
 
+def test_re_PostSB():
+    text = 'LordShiva posts small blind 0.01€'
+    match = re_PostSB.search(text)
+    assert match is not None
+    assert match.group('PNAME') == 'LordShiva'
+    assert match.group('SB') == '0.01'
+
+
+re_PostBB    = re.compile(r"(?P<PNAME>.*?)\sposts\sbig\sblind\s(%(LS)s)?(?P<BB>[\.0-9]+)(%(LS)s)?" % substitutions, re.MULTILINE)
+
+def test_re_PostBB():
+    text = 'LordShiva posts big blind 0.01€'
+    match = re_PostBB.search(text)
+    assert match is not None
+    assert match.group('PNAME') == 'LordShiva'
+    assert match.group('BB') == '0.01'
+
+re_PlayerInfo        = re.compile(r"Seat\s(?P<SEAT>[0-9]+):\s(?P<PNAME>.*)\s\((%(LS)s)?(?P<CASH>[.0-9]+)(%(LS)s)?(,\s(%(LS)s)?(?P<BOUNTY>[.0-9]+)(%(LS)s)?\sbounty)?\)" % substitutions)
+
+def test_re_PlayerInfo():
+    text = 'Seat 1: 77boy77 (0.60€)'
+    match = re_PlayerInfo.search(text)
+    assert match is not None
+    assert match.group('SEAT') == '1'
+    assert match.group('PNAME') == '77boy77'
+    assert match.group('CASH') == '0.60'
