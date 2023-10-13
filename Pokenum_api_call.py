bb582211f632ea93a495d150598fe3b6162ed5b5
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2023-10-13 09:34:08 +0200

    add monte carlo method

--- Pokenum_api_call.py
+++ Pokenum_api_call.py
@@ -1,5 +1,6 @@
 import requests
 from pydantic import BaseModel
+import json
 
 class PokenumRequest(BaseModel):
     game: str
@@ -15,17 +16,18 @@ endpoint = "/pokenum"
 
 # Create a PokenumRequest object with the desired parameters
 request_body = PokenumRequest(
+    method="-mc",
+    iterations="10000",
     game="-h",
     hand=['As', 'Ad', '-', 'Ks', 'Qs'],
     board=["--"],
     dead=["/"],
-    method="",
-    iterations="",
     histogram=False
 )
 
 # Send a POST request to the API with the request body
 response = requests.post(f"{app_url}{endpoint}", json=request_body.dict())
-
+content = response.content.decode("utf-8")
+data_dict = json.loads(content)
 # Print the response content
-print(response.content)
+print(data_dict)
