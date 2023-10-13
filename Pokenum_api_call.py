ee5fa7cf206d4e3939fd918b2c46912312fc5973
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2023-10-13 15:00:05 +0200

    mofify and add examples call pokenum

--- Pokenum_api_call.py
+++ Pokenum_api_call.py
@@ -14,20 +14,22 @@ class PokenumRequest(BaseModel):
 app_url = "http://localhost:8000"  # Replace with the actual URL of the API
 endpoint = "/pokenum"
 
-# Create a PokenumRequest object with the desired parameters
-request_body = PokenumRequest(
-    method="-mc",
-    iterations="10000",
-    game="-h",
-    hand=['As', 'Ad', '-', 'Ks', 'Qs'],
-    board=["--"],
-    dead=["/"],
-    histogram=False
-)
+def run_pokenum(method, iterations, game, hand, board, dead):
+    # Create a PokenumRequest object with the desired parameters
+    request_body = PokenumRequest(
+        method=method,
+        iterations=iterations,
+        game=game,
+        hand=hand,
+        board=board,
+        dead=dead,
+        histogram=False
+    )
 
-# Send a POST request to the API with the request body
-response = requests.post(f"{app_url}{endpoint}", json=request_body.dict())
-content = response.content.decode("utf-8")
-data_dict = json.loads(content)
-# Print the response content
-print(data_dict)
+    # Send a POST request to the API with the request body
+    response = requests.post(f"{app_url}{endpoint}", json=request_body.dict())
+    content = response.content.decode("utf-8")
+    data_dict = json.loads(content)
+    # Return the response content
+    return data_dict
+
