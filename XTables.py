50fe5be701b5dcc46d168b18d062c9a0637ca11f
Bruno Duyé <86322+brunetton@users.noreply.github.com>
2024-10-23 20:40:13 +0200

    XTables - replace print() by log.debug()

--- XTables.py
+++ XTables.py
@@ -65,7 +65,7 @@ class Table(Table_Window):
         wins = xconn.core.GetProperty(False, root, nclatom, winatom, 0, (2**32) - 1).reply().value.to_atoms()
         for win in wins:
             w_title = xconn.core.GetProperty(False, win, wnameatom, utf8atom, 0, (2**32) - 1).reply().value.to_string()
-            print("w_title:", w_title)
+            log.debug("w_title:", w_title)
             # escaped_search_string = re.escape(self.search_string)
             # if re.search(escaped_search_string, w_title, re.I):
             if re.search(self.search_string, w_title, re.I):
@@ -75,9 +75,9 @@ class Table(Table_Window):
                 if self.check_bad_words(title):
                     continue
                 self.number = win
-                print("self.number:", self.number)
+                log.debug("self.number:", self.number)
                 self.title = title
-                print("self.title:", self.title)
+                log.debug("self.title:", self.title)
                 break
 
         if self.number is None:
@@ -94,7 +94,7 @@ class Table(Table_Window):
         try:
             geo = xconn.core.GetGeometry(self.number).reply()
             absxy = xconn.core.TranslateCoordinates(self.number, root, geo.x, geo.y).reply()
-            # print('coord:', absxy.dst_x, absxy.dst_y)
+            # log.debug('coord:', absxy.dst_x, absxy.dst_y)
             return {"x": absxy.dst_x, "y": absxy.dst_y, "width": geo.width, "height": geo.height}
         except xcffib.xproto.DrawableError:
             return None
