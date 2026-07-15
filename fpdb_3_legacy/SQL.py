6d10d6be467bb989ecf3f12aa06361920b4883b8
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2026-07-15 15:38:02 +0200

    Extract hand detail SQL queries

--- fpdb_3_legacy/SQL.py
+++ fpdb_3_legacy/SQL.py
@@ -9,6 +9,7 @@ import sys
 from fpdb_3_legacy.sql_indexes import index_queries
 from fpdb_3_legacy.sql_metadata import metadata_queries
 from fpdb_3_legacy.sql_queries_core import core_lookup_queries
+from fpdb_3_legacy.sql_queries_hand_detail import hand_detail_queries
 from fpdb_3_legacy.sql_schema_cards_cache import cards_cache_schema_queries
 from fpdb_3_legacy.sql_schema_core import core_schema_queries
 from fpdb_3_legacy.sql_schema_game import game_schema_queries
@@ -79,6 +80,7 @@ class Sql:
         self.query.update(time_schema_queries(db_server))
         self.query.update(index_queries(db_server))
         self.query.update(core_lookup_queries())
+        self.query.update(hand_detail_queries())
         ###############################################################################3
         #    Support for the Free Poker DataBase = fpdb   http://fpdb.sourceforge.net/
         #
@@ -1124,97 +1126,6 @@ sum(hc.street0Limp)                 AS limp,
                        there's a gap over X minutes between hands (ie. when we get back to start of
                        the session */
                 """
-
-        self.query["get_players_from_hand"] = """
-                SELECT HandsPlayers.playerId, seatNo, name
-                FROM  HandsPlayers INNER JOIN Players ON (HandsPlayers.playerId = Players.id)
-                WHERE handId = %s
-            """
-        #                    WHERE handId = %s AND Players.id LIKE %s
-
-        self.query["get_winners_from_hand"] = """
-                SELECT name, winnings
-                FROM HandsPlayers, Players
-                WHERE winnings > 0
-                    AND Players.id = HandsPlayers.playerId
-                    AND handId = %s;
-            """
-
-        self.query["get_table_name"] = """
-                SELECT h.tableName, gt.maxSeats, gt.category, gt.type, gt.fast, s.id, s.name
-                     , count(1) as numseats
-                FROM Hands h, Gametypes gt, Sites s, HandsPlayers hp
-                WHERE h.id = %s
-                    AND   gt.id = h.gametypeId
-                    AND   s.id = gt.siteID
-                    AND   hp.handId = h.id
-                GROUP BY h.tableName, gt.maxSeats, gt.category, gt.type, gt.fast, s.id, s.name
-            """
-
-        self.query["get_actual_seat"] = """
-                select seatNo
-                from HandsPlayers
-                where HandsPlayers.handId = %s
-                and   HandsPlayers.playerId  = (select Players.id from Players
-                                                where Players.name = %s)
-            """
-
-        self.query["get_cards"] = """
-/*
-    changed to activate mucked card display in draw games
-    in draw games, card6->card20 contain 3 sets of 5 cards at each draw
-
-    CASE code searches from the highest card number (latest draw) and when
-    it finds a non-zero card, it returns that set of data
-*/
-            SELECT
-                seatNo AS seat_number,
-                CASE Gametypes.base
-                    when 'draw' then COALESCE(NULLIF(card16,0), NULLIF(card11,0), NULLIF(card6,0), card1)
-                    else card1
-                end card1,
-                CASE Gametypes.base
-                    when 'draw' then COALESCE(NULLIF(card17,0), NULLIF(card12,0), NULLIF(card7,0), card2)
-                    else card2
-                end card2,
-                CASE Gametypes.base
-                    when 'draw' then COALESCE(NULLIF(card18,0), NULLIF(card13,0), NULLIF(card8,0), card3)
-                    else card3
-                end card3,
-                CASE Gametypes.base
-                    when 'draw' then COALESCE(NULLIF(card19,0), NULLIF(card14,0), NULLIF(card9,0), card4)
-                    else card4
-                end card4,
-                CASE Gametypes.base
-                    when 'draw' then COALESCE(NULLIF(card20,0), NULLIF(card15,0), NULLIF(card10,0), card5)
-                    else card5
-                end card5,
-                CASE Gametypes.base
-                    when 'draw' then 0
-                    else card6
-                end card6,
-                CASE Gametypes.base
-                    when 'draw' then 0
-                    else card7
-                end card7
-
-                FROM HandsPlayers, Hands, Gametypes
-                WHERE handID = %s
-                 AND HandsPlayers.handId=Hands.id
-                 AND Hands.gametypeId = Gametypes.id
-                ORDER BY seatNo
-            """
-
-        self.query["get_common_cards"] = """
-                select
-                boardcard1,
-                boardcard2,
-                boardcard3,
-                boardcard4,
-                boardcard5
-                from Hands
-                where Id = %s
-            """
 
         if db_server == "mysql":
             self.query["get_hand_1day_ago"] = """
