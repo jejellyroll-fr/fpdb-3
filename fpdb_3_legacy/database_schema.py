68a58355a987452c52707dd51257ceeaa3df3487
jejellyroll-fr <47847785+jejellyroll-fr@users.noreply.github.com>
2026-09-18 16:54:07 +0200

    Make hand-state rebuilds complete and paginated

--- fpdb_3_legacy/database_schema.py
+++ fpdb_3_legacy/database_schema.py
@@ -1338,6 +1338,7 @@ class DatabaseSchemaMixin:
         c.execute(self.sql.query["createHandsShowdownTable"])
         c.execute(self.sql.query["createHandsCashoutTable"])
         c.execute(self.sql.query["createHandsSituationsTable"])
+        c.execute(self.sql.query["createHandStatesTable"])
         c.execute(self.sql.query["createPlayerAutoNotesTable"])
         c.execute(self.sql.query["createAofDecisionsTable"])
         c.execute(self.sql.query["createAofDecisionAnalysesTable"])
